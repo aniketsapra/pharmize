e6c5c67d9d63dfcb5a88d392cef036f53f0d5d2c
Aniket Sapra <aniketsapra2000@gmail.com>
2025-05-23 01:15:29 +0530

    database-url-change

--- backend/database.py
+++ backend/database.py
@@ -6,9 +6,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
